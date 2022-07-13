0ddd41d41ac29274251f689b60fef5891bbca801
JossenRinn <jossenrinn3@gmail.com>
2022-07-13 15:21:30 -0500

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'dgcv1-23370',
+    appId: '1:473163664221:web:88069d488a6f7814c7b6b4',
+    storageBucket: 'dgcv1-23370.appspot.com',
+    apiKey: 'AIzaSyAgffiirG_M85xZBi2vkELpJfclZFeWlH4',
+    authDomain: 'dgcv1-23370.firebaseapp.com',
+    messagingSenderId: '473163664221',
+    measurementId: 'G-T499SEQGYY',
+  },
   production: true
 };
