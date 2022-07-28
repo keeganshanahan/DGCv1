0cc017ca562a05e19c74742d896fa2557f1074a9
JacksonWard55 <jacksonward@unomaha.edu>
2022-07-28 14:39:02 -0500

    firebase finally works

--- src/app/intake-form/intake-form.page.ts
+++ src/app/intake-form/intake-form.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Route, Router } from '@angular/router';
 import { format } from 'path';
 
 @Component({
@@ -17,20 +18,44 @@ export class IntakeFormPage {
       { val: '  Tour or Demo', isChecked: true },
       { val: '  Other', isChecked: true }
     ];
-  inputVal1: string
-  db: any;
+  inputValName: string = ""
+  inputValNumber: string = ""
+  inputValAddress: string = ""
+  inputValTitle: string = ""
+  inputVal1: boolean = true;
+ inputVal2: boolean=true
+ inputVal3: boolean=true
+ inputVal4: boolean=true
+ inputVal5: boolean=true
+ inputVal6: boolean=true
+ inputVal7: boolean=true
   
-submit() {
-  window.location.reload()
-  const collectionRef = this.db.collection('Info')
-  collectionRef
-  .doc()
-  .set({ Text: this.inputVal1})
-
-}
 
 
-  constructor(private route: Router) {}
+
+  constructor(private route: Router, public db: AngularFirestore) {}
+
+  submit() {
+    // window.location.reload()
+    const collectionRef = this.db.collection('Info')
+    collectionRef
+    .doc()
+    .set({ Name: this.inputValName, 
+      Number: this.inputValNumber, 
+      Address: this.inputValAddress, 
+      Title: this.inputValTitle,
+      Visualization: this.inputVal1,
+      ClinicalSimulation: this.inputVal2,
+      HumanSimulation: this.inputVal3,
+      SurgicalSimulation: this.inputVal4,
+      EducationalResearch: this.inputVal5,
+      TourOrDemo: this.inputVal6,
+      Other: this.inputVal7
+  }).catch(err => {
+      console.log(err)
+    })
+  
+  }
   cancel() {
     this.route.navigate(['/home']);
   }
