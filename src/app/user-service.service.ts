6556b92ec87d3bd45af4662605d0151c92334286
JossenRinn <jossenrinn3@gmail.com>
2022-07-29 13:43:18 -0500

    user service

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -4,13 +4,20 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Route, Router } from '@angular/router';
 import { format } from 'path';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { getAdditionalUserInfo } from 'firebase/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
-  FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-  String uid = user.getUid();
-  constructor() { }
+
+  constructor(public angularFireAuth: AngularFireAuth) { }
+
+  async getUser(){
+    var user = await this.angularFireAuth.currentUser;
+    var uid = user.uid
+  }
 }
+
+
 //need get and set methods within other branch (log-in)
